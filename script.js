53836c60880eba0c798fadfb0f0a4d671615f72e
frezzz96 <skonovalov489@gmail.com>
2024-09-29 23:46:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,30 @@ const images = [
     '16.jpg',
     '17.jpg',
     '18.jpg',
-    '19.jpg'
+    '19.jpg',
+    '20.jpg',
+    '20.jpg',
+    '21.jpg',
+    '22.jpg',
+    '23.jpg',
+    '24.jpg',
+    '25.jpg',
+    '26.jpg',
+    '27.jpg',
+    '28.jpg',
+    '29.jpg',
+    '30.jpg',
+    '31.jpg',
+    '32.jpg',
+    '33.jpg',
+    '34.jpg',
+    '35.jpg',
+    '36.jpg',
+    '37.jpg',
+    '38.jpg',
+    '39.jpg',
+    '40.jpg'
+    
     // Add more image URLs here
 ];
 showImageBtn.addEventListener('click', () => {
